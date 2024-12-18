d5d4c4cb18fdee7a42460bbcc5a269bf8267573a
rexlx <rxlx@nullferatu.com>
2024-12-18 10:00:42 -0600

    visited by the whale

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Start the server in first user mode
+/server -firstuse &
+
+# Wait for the server to start
+sleep 3
+
+# Add the initial user
+curl -X POST http://localhost:8080/adduser -d '{"email": "admin@aol.com", "password": "admin"}'
+
+# Stop the server
+killall server
+
+# Wait for the server to stop
+sleep 2
+
+# Restart the server in normal mode
+/server
