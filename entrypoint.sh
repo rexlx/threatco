22a3bcb16576c2a9378628406686105c765af8cc
rexlx <rxlx@nullferatu.com>
2025-06-11 16:44:42 -0500

    bn

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@
 sleep 3
 
 # Add the initial user
-curl -X POST http://localhost:8080/adduser -d '{"email": "admin@aol.com", "password": "admin"}'
+curl -X POST http://localhost:8081/adduser -d '{"email": "admin@aol.com", "password": "admin"}'
 
 # Stop the server
 killall server
