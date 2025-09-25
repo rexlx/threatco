7816c927f387b26c01046b8276024c56fbde21af
rexlx <rxlx@nullferatu.com>
2025-09-25 08:37:48 -0500

    add prompt stub

---
+++ optional/prompt.go
@@ -0,0 +1,23 @@
+package optional
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const BasicPrompt = `You are an intel analyst. Your task is to analyze the indicators of compromise (IOCs) provided in the input anf further enrich them if possible
+and to categorize them by how significant the threat is. %v`
+
+type PromptRequest struct {
+	Id           string        `json:"id"`
+	MatchList    []interface{} `json:"match_list"`
+	TransactinID string        `json:"transaction_id"`
+}
+
+func (p *PromptRequest) BuildPrompt() (string, error) {
+	out, err := json.Marshal(p.MatchList)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf(BasicPrompt, string(out)), nil
+}
